bff57a32aa0032c8f8f9271e20869c0248397e2a
cmda12 <dam.carrera@yavirac.edu.ec>
2024-07-17 23:14:55 -0500

    fix: Remove name a view CV

--- src/pages/modules/presentation/PresentationScreen.tsx
+++ src/pages/modules/presentation/PresentationScreen.tsx
@@ -9,7 +9,7 @@ import "./styles.css"; // Archivo CSS para estilos y animaciones
 const PresentationScreen: React.FC = () => {
   const { isDarkTheme } = useTheme();
   const { translations } = useLanguage();
-  
+
   const [currentImg, setCurrentImg] = useState(personaImg);
 
   const handleMouseEnter = () => {
@@ -27,11 +27,10 @@ const PresentationScreen: React.FC = () => {
   return (
     <PageWrapperCustom>
       <div
-        className={`flex flex-col md:flex-row mx-4 ${
-          isDarkTheme
-            ? "bg-dark-primary text-white"
-            : "bg-light-primary text-black"
-        }`}
+        className={`flex flex-col md:flex-row mx-4 ${isDarkTheme
+          ? "bg-dark-primary text-white"
+          : "bg-light-primary text-black"
+          }`}
         style={{ fontFamily: "Times New Roman, Times, serif" }}
       >
         <div className="flex-1 flex justify-center items-center order-1 md:order-2 mt-4 md:mt-3">
@@ -68,7 +67,11 @@ const PresentationScreen: React.FC = () => {
               onClick={handleDownloadCV}
               className={`mt-2 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition duration-300`}
             >
-              <a href="https://drive.google.com/file/d/1sQeb7bWDa3wCWM6sOFZXA56s8GF6O_Cb/view?usp=sharing" target="_blank">{translations['presentation.download_cv']}</a>
+              <a href="https://drive.google.com/file/d/1sQeb7bWDa3wCWM6sOFZXA56s8GF6O_Cb/view?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {translations['presentation.download_cv']}</a>
             </button>
           </div>
         </div>
